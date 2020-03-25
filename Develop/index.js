525fef9297b3a949002e310ba1c8b79581d98e9b
Ricardo Russ <ricardoruss@live.com>
2020-03-25 19:58:04 -0400

    adding in working inquirer function for modification

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,32 @@
+var inquirer = require("inquirer");
+
 const questions = [
 
 ];
 
 function writeToFile(fileName, data) {
 }
+
+
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username"
+    },
+    {
+      type: "password",
+      message: "What is your password?",
+      name: "password"
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm"
+    }
+  ])
 
 function init() {
 
