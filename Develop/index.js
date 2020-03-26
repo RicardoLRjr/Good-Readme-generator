2215fd75fb3361be3c1c76928eec184c6a4613cd
Ricardo Russ <ricardoruss@live.com>
2020-03-25 20:18:37 -0400

    added in readme parameters

--- Develop/index.js
+++ Develop/index.js
@@ -13,19 +13,45 @@ inquirer
   .prompt([
     {
       type: "input",
-      message: "What is your user name?",
-      name: "username"
+      message: "What is your project's name?",
+      name: "projectName"
     },
     {
-      type: "password",
-      message: "What is your password?",
-      name: "password"
+        type: "input",
+        message: "Give a short description of your project.",
+        name: "projectDescription"
+    },
+
+    // installation goes here
+
+    {
+        type: "input",
+        message: "How is your project used?",
+        name: "usage"
+    },
+
+    {
+        type: "input",
+      message: "What license is your project working under?",
+      name: "license"
+    },
+
+    {
+        type: "input",
+      message: "Who contributed to this project?",
+      name: "contributingAuthors"
     },
     {
-      type: "password",
-      message: "Re-enter password to confirm:",
-      name: "confirm"
+        type: "input",
+      message: "Which tests can you use here?",
+      name: "tests"
+    },
+    {
+        type: "input",
+      message: "Any last questions?",
+      name: "questions"
     }
+
   ])
 
 function init() {
